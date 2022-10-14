90917a1e9a253c1b852417bf5a48626e08a2977a
tak <wontaek22@gmail.com>
2022-10-14 22:21:45 +0900

    22.10.14
    int형 변환에 실숫값을 대입하여 표시

---
+++ src/com/tak/study/Study.java
@@ -0,0 +1,15 @@
+package com.tak.study;
+
+public class Study {
+
+    public static void main(String[] args) {
+
+        int a;
+
+        a = (int) 10.0;
+//      a = 10.0;           //타입에 맞지않아 오류가 뜬다. 위의 방식으로 변환을 해주면 된다.
+
+        System.out.println("a = " + a);
+    }
+
+}
